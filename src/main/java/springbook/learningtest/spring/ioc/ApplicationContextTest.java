8124f7aa9209bd4bf1d3917613900fa112f47fc9
ourhome527-max <ourhome527@gmail.com>
2025-11-24 00:17:30 +0900

    second commit

--- src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -6,9 +6,13 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.StringPrinter;
 
 public class ApplicationContextTest {
 
@@ -19,5 +23,32 @@ public class ApplicationContextTest {
 
 		Hello hello1 = ac.getBean("hello1", Hello.class);
 		assertThat(hello1, is(notNullValue()));
+
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		ac.registerBeanDefinition("hello2", helloDef);
+
+		Hello hello2 = ac.getBean("hello2", Hello.class);
+		assertThat(hello2.sayHello(), is("Hello Spring"));
+
+		assertThat(hello1, is(not(hello2)));
+
+	}
+
+	@Test
+	public void registerBeanWithDependency() {
+		StaticApplicationContext ac = new StaticApplicationContext();
+
+		ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class)); // StringPrinter 클래스 타입이며
+																							// printer라는 이름을 가진 빈을 등록
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring"); // 단순 값을 갖는 프로퍼티 등록
+		helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
+
+		ac.registerBeanDefinition("hello", helloDef);
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
 	}
 }
